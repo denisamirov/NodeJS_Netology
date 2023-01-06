8515fec2ee5974fab54d4e332f41d14c2ac6fc2c
denis <CyberTatt@yandex.ru>
2023-01-06 12:48:33 +0300

    005-http

---
+++ 005-http/index.js
@@ -0,0 +1,25 @@
+//npm run dev city
+const http = require('http');
+const myAPIkey = process.env.myAPIkey;
+const city = process.argv[2];
+
+const url = `http://api.weatherstack.com/current?access_key=${myAPIkey}&query=${city}`;
+
+
+http.get(url, (res) => {
+    const {statusCode} = res;
+    if (statusCode !== 200){
+        console.log(`statusCode: ${statusCode}`)
+        return
+    }
+
+    res.setEncoding('utf8')
+    let rowData = ''
+    res.on('data', (chunk) => rowData += chunk)
+    res.on('end', () => {
+        let parseData = JSON.parse(rowData)
+        console.log(parseData)
+    })
+}).on('error', (err) => {
+    console.error(err)
+})
